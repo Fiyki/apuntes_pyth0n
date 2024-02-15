79ecced0a618ac56526afeffaf728819855b61a5
danmo <danmon1b@gmail.com>
2024-02-15 16:30:22 +0100

    Graficas mejoradas

--- Tema  5/Graficos.py
+++ Tema  5/Graficos.py
@@ -10,18 +10,44 @@ df = pd.read_csv(fichero_csv)  # Carga el archivo CSV en un DataFrame
 # Gráfico de líneas
 ventas_mes = df.groupby(['Mes'], sort=False).sum()  # Agrupar los datos por mes y sumar las unidades vendidas
 grafico_lineas = ventas_mes.plot(y='Unidades', color="red")  # Crear el gráfico de líneas utilizando matplotlib
-grafico_datapane_lineas = dp.Plot(grafico_lineas, responsive=False)  # Convertir el gráfico de matplotlib a un objeto Plot de Datapane
+grafico_datapane_lineas = dp.Plot(grafico_lineas,
+                                  responsive=False)  # Convertir el gráfico de matplotlib a un objeto Plot de Datapane
 
 # Gráfico de barras
 ventas_vendedor = df.groupby(['Nombre']).sum()  # Agrupar los datos por nombre del vendedor y sumar los importes
-grafico_barras = ventas_vendedor.plot.bar(y='Importe (€)', color="yellow")  # Crear el gráfico de barras utilizando matplotlib
+grafico_barras = ventas_vendedor.plot.bar(y='Importe (€)',
+                                          color="yellow")  # Crear el gráfico de barras utilizando matplotlib
 plt.tight_layout()  # Ajustar el diseño del gráfico para evitar que se corten las etiquetas
-grafico_datapane_barras = dp.Plot(grafico_barras, responsive=False)  # Convertir el gráfico de matplotlib a un objeto Plot de Datapane
+grafico_datapane_barras = dp.Plot(grafico_barras,
+                                  responsive=False)  # Convertir el gráfico de matplotlib a un objeto Plot de Datapane
 
 # Gráfico de sectores
-grafico_sectores = ventas_vendedor.plot.pie(y='Unidades', legend=False, ylabel="", cmap='viridis')  # Crear el gráfico de sectores utilizando matplotlib
-grafico_datapane_sectores = dp.Plot(grafico_sectores, responsive=False)  # Convertir el gráfico de matplotlib a un objeto Plot de Datapane
+grafico_sectores = ventas_vendedor.plot.pie(y='Unidades', legend=False, ylabel="",
+                                            cmap='viridis')  # Crear el gráfico de sectores utilizando matplotlib
+grafico_datapane_sectores = dp.Plot(grafico_sectores,
+                                    responsive=False)  # Convertir el gráfico de matplotlib a un objeto Plot de Datapane
 
 # Crear el informe que contiene los gráficos
-report = dp.Report(grafico_datapane_lineas, grafico_datapane_barras, grafico_datapane_sectores)  # Guardar el informe como un archivo HTML y abrirlo
-report.save(path='Graficos.html', open=True)
+report = dp.Report(
+    dp.Group(
+        grafico_datapane_lineas,
+        grafico_datapane_barras,
+        grafico_datapane_sectores,
+        columns=2
+    )
+)
+# Agrupar por Páginas
+report = dp.Report(
+    dp.Page(
+        title="Página 1",
+        blocks=[grafico_datapane_lineas, grafico_datapane_barras]
+    ),
+    dp.Page(
+        title="Página 2",
+        blocks=[grafico_datapane_sectores, grafico_datapane_lineas]
+    ),
+    dp.Page(
+        title="Página 3",
+        blocks=[grafico_datapane_barras, grafico_datapane_sectores]
+    ),
+)
